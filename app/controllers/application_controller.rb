1f5bdf923422b3f6939c16c13f0e0a23f0dccab4
dieulinh <linhvn09@gmail.com>
2015-07-13 02:31:25 +0700

    process language settings with cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
  
 	def set_locale
-	  I18n.locale = params[:locale] || I18n.default_locale
+		if cookies[:post_locale] && I18n.available_locales.include?(cookies[:post_locale].to_sym)
+    	l = cookies[:post_locale].to_sym
+    else
+    	l = I18n.default_locale
+    	cookies.permanent[:post_locale] = l
+	  end
+	  I18n.locale = l
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :new, :create]
   root 'posts#index'
+  get '/change_locale/:locale', to: 'settings#change_locale', as: :change_locale
 end
