a81366d3944a30d3e27aa0a9eb335a0b21e72055
dieulinh <linhvn09@gmail.com>
2015-07-13 01:13:18 +0700

    using bootstrap-sass and preparing data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,28 @@
 class PostsController < ApplicationController
 	def index
+		@posts = Post.all
+	end
+
+	def new
+		@product = Post.new
+	end
+
+	def create
+		@post = Post.find(post_id)
+		if @post.save
+			redirect_to posts_path_url
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def post_id
+		params[:id]
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :body)
 	end
 end
